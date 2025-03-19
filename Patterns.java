69633857043a758e39f8406ddf03c73f69278a26
Arul A <nareshlaesn170705@gmail.com>
2025-03-19 21:44:45 +0530

    19-3-25

--- Patterns.java
+++ Patterns.java
@@ -221,3 +221,116 @@ public class Solution {
         }
     }
 }
+
+/*
+   1 
+  1 2 
+ 1 2 3 
+1 2 3 4 
+ 1 2 3 
+  1 2 
+   1 
+ */
+
+ public class Main {
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        for(int i=n-2;i>=1;i--)
+        {
+            for(int j=1;j<n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ *            *
+**          **
+***        ***
+****      ****
+*****    *****
+******  ******
+**************
+*****    *****
+****      ****
+***        ***
+**          **
+*            * 
+ 
+ */
+
+ 
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 1; i <=n; i++) {
+            for (int j = 1; j <= 2 * n; j++) {
+                if (j > i && j <= 2 * n - i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            for (int j = 1; j <= 2 * n; j++) {
+                if (j > i && j <= 2 * n - i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ ******
+******
+******
+******
+******
+******
+ */
+
+ 
+import java.util.*;
+class Mian
+{
+    public static void main(String[] args) {
+        int n=6;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
