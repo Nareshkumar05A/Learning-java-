0bdfb4731a63087650422bf0d10c861c21758639
Arul A <nareshlaesn170705@gmail.com>
2025-03-22 21:35:34 +0530

    22-3-25

--- Patterns.java
+++ Patterns.java
@@ -334,3 +334,31 @@ class Mian
         }
     }
 }
+
+/*     1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+ */
+
+
+public class Pattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+
+            for (int k = n; k > i; k--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+            }
+            System.out.println();
+        }
+    }
+}
