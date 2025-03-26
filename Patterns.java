5845d27bc8a6006b1fde7c9753e19340abd80651
Arul A <nareshlaesn170705@gmail.com>
2025-03-26 19:17:26 +0530

    26-3-25

--- Patterns.java
+++ Patterns.java
@@ -395,3 +395,37 @@ public class Solution {
         }
     }
 }
+
+
+/* 
+        *    
+      * * *  
+    * * * * *  
+  * * * * * * *  
+* * * * * * * * *  
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+  /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+  Scanner scan = new Scanner(System.in);
+  int n = scan.nextInt();
+  for(int i = 1; i <= n; i++)
+  {
+      for(int j = 1; j <= n -i; j++)
+      {
+          System.out.print("  ");
+      }
+      for(int j = 0; j < 2 * i - 1; j++)
+      {
+          System.out.print("* ");
+      }
+      System.out.println();
+  }
+}
+}
